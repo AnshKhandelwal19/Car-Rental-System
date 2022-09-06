c47eacac2e0ceb87ff97b8b45b89da69c7fd7eb7
Ansh <khandelwalansh8@gmail.com>
2022-09-06 16:54:08 -0700

    Added a.out to gitignore

--- system.cpp
+++ system.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include "login.h"
+
+using namespace std;
+struct Day{
+    int month;
+    int day;
+    int year;
+};
+struct Car {
+    string name;
+    double msrp;
+    double rent_cost_per_hour;
+};
+struct Renter{
+    string name;
+
+};
 
 int main() {
 
-    
-    
+    if(!login()) {
+        exit(1);
+    }
+
     return 0;
 }
