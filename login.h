49402d0c0dc024c70a1688341b618b888ae43eeb
Ansh <khandelwalansh8@gmail.com>
2022-09-08 10:09:41 -0700

    Added function to register users

--- login.h
+++ login.h
@@ -4,29 +4,29 @@
 
 using namespace std;
 
-int findUser(vector<Employee> e, string findUsn) {
-    for(int i = 0; i < e.size(); i++) {
-        if(e[i].usn == findUsn) {
+vector<Employee> employee_database;
+
+int findUser(string findUsn) {
+    for(int i = 0; i < employee_database.size(); i++) {
+        if(employee_database[i].usn == findUsn) {
             return i;
         }
     }
     return -1;
 }
 
-vector<Employee> createEmployeeLogin(vector<string> loginInfo) {
-    vector<Employee> rV;
+void createEmployeeDatabase(vector<string> loginInfo) {
     for(int i = 0; i < loginInfo.size(); i++) {
         Employee e = {loginInfo[i].substr(0, loginInfo[i].find(",")), loginInfo[i].substr(loginInfo[i].find(",")+1)};
-        rV.push_back(e);
+        employee_database.push_back(e);
     }
-    return rV;
 }
 
 bool login() {
     bool successfulLogin = false;
     //Read admin text file to get username and passwords to allow to login to system
-    vector<Employee> e = createEmployeeLogin(readFile("admin.txt"));
-    if(e.size() == 0) {
+    createEmployeeDatabase(readFile("admin.txt"));
+    if(employee_database.size() == 0) {
         exit(1);
     }
 
@@ -37,12 +37,12 @@ bool login() {
     while(true) {
         cout << "Username: ";
         cin >> usn;
-        pos = findUser(e, usn);
+        pos = findUser(usn);
         if(pos != -1) {
             while(count < 3) {
                 cout << "Password: ";
                 cin >> psw;
-                if(e[pos].psw == psw) {
+                if(employee_database[pos].psw == psw) {
                     return true;
                 }
                 else {
