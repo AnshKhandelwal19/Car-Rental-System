98e56543c2b470009878dc33c993a6d0f8419f74
Ansh <khandelwalansh8@gmail.com>
2022-09-13 11:41:05 -0700

    Put headers files into a folder

---
+++ HeaderFiles/empFunctions.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include "login.h"
+
+using namespace std;
+
+void registerEmp() {
+    string usn, psw, verify_psw;
+    bool keepRunning;
+    //Register username
+    do {
+        keepRunning = false;
+        cout << "Username: ";
+        cin >> usn;
+        if(findUser(usn) != -1) {
+            cout << "Username already in use." << endl;
+            keepRunning = true;
+        }
+    }while(keepRunning);
+    keepRunning = false;
+    
+    //Add Password
+    do {
+        keepRunning = false;
+        cout << "Password: ";
+        cin >> psw;
+        cout << "Confirm password: ";
+        cin >> verify_psw;
+        if(psw != verify_psw) {
+            cout << "Passwords do not match." << endl;
+            keepRunning = true;
+        }
+    }while(keepRunning);
+
+    Employee e = {usn, psw};
+    employee_database.push_back(e);
+    writeToEmployeeDatabase(e);
+
+    cout << "User " << usn << " has been added." << endl;
+}
+
+void removeEmp(string usn) {
+    
+}
---
+++ HeaderFiles/inventory.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Car {
+    string name;
+    string style; // Sedan/Coupe/SUV
+    double rent_cost_per_day;
+};
+
+vector<Car> createInventory() {
+    vector<Car> rV;
+
+    
+
+    return rV;
+}
